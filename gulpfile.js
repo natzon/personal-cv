65d8bbe18369d08b7784e9c21796dcd3d1a435ca
natzon <nanzhu168@163.com>
2017-09-20 00:11:05 +0800

    v2.0 add section2

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,6 @@ gulp.task('testLess', function () {
 gulp.task('default',['testLess'],function() {
   // 将你的默认的任务代码放在这
 });
+gulp.task('testWatch', function () {
+    gulp.watch('less/*.less', ['testLess']); //当所有less文件发生改变时，调用testLess任务
+});
